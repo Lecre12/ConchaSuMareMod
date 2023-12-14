c9ebffee688d65c53345d51ac1f7100ebf24c437
Adrián <124732460+Lecre12@users.noreply.github.com>
2023-12-14 21:19:11 +0100

    Agregar archivos de proyecto.

---
+++ migerMod/Patches/Fallvoid.cs
@@ -0,0 +1,84 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ConchaSuMare.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class FallvoidPatch : HarmonyPatch
+    {
+        static bool done;
+        static AudioClip newSFX;
+        static ManualLogSource mls;
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void prepareAudio()
+        {
+            done = false;
+            mls = BepInEx.Logging.Logger.CreateLogSource("Lecre.conchaSuMareMod");
+            // Load the audio file
+            string location = ((BaseUnityPlugin)ConchaSuMare.instance).Info.Location;
+            string text = "conchaSuMare.dll";
+            string text2 = location.TrimEnd(text.ToCharArray());
+            string path = text2 + "CONCHA.wav";
+            ((MonoBehaviour)ConchaSuMare.instance).StartCoroutine(LoadAudio("file:///" + path, clip =>
+            {
+                newSFX = clip;
+            }));
+        }
+
+        static IEnumerator LoadAudio(string url, Action<AudioClip> callback)
+        {
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.WAV))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    mls.LogError("Failed to load audio assets!");
+                }
+                else
+                {
+                    AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
+                    if (clip == null)
+                    {
+                        mls.LogError("The audio clip is null after loading!");
+                    }
+                    else
+                    {
+                        callback(clip);
+                        mls.LogInfo("Audio inserted");
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+
+        static void SoundVoidPatch(ref PlayerControllerB __instance)
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("Lecre.conchaSuMareMod");
+            PlayerControllerB playerRef = __instance;
+            CauseOfDeath causeOfDeath = __instance.causeOfDeath;
+            if (playerRef.isPlayerDead && !done && (causeOfDeath == CauseOfDeath.Gravity || causeOfDeath == CauseOfDeath.Unknown))
+            {
+                done = true;
+                // Play the audio file
+                AudioSource audioSource = playerRef.gameObject.AddComponent<AudioSource>();
+                audioSource.clip = newSFX;
+                audioSource.Play();
+                mls.LogInfo("CONCHA TU MAIIII, one memeber of the crew fell to the void, what a loser xD");
+            }
+            
+        }
+
+    }
+
+}
---
+++ migerMod/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ConchaSuMare.Patches;
+
+namespace ConchaSuMare
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ConchaSuMare : BaseUnityPlugin
+    {
+        private const string modGUID = "Lecre.ConchaSuMareMod";
+        private const string modName = "LC Mod test";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static ConchaSuMare instance;
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+
+            if (instance == null)
+            {
+                instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Lecre.migerMod is loading");
+
+            harmony.PatchAll(typeof(ConchaSuMare));
+            harmony.PatchAll(typeof(FallvoidPatch));
+        }
+
+    }
+}
